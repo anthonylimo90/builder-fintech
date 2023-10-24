77ced1c49796875a907760b5a1f844875f3ba20e
Anthony Kiplimo <anthonylimo90@gmail.com>
2023-10-24 14:19:17 +0300

    Fixed bugs

--- utils/customer.js
+++ utils/customer.js
@@ -27,7 +27,7 @@ const customer = {
     
         console.log(customerResp.data);
     
-        return JSON.stringify(customerResp.data);
+        return customerResp.data;
     },
     walletQuery: async (phoneNumber) => {
 
@@ -55,7 +55,7 @@ const customer = {
     
         console.log(customerResp.data);
     
-        return JSON.stringify(customerResp.data);
+        return customerResp.data;
     
     
     },
